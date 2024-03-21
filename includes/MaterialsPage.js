dbd32a3e4c5d07d4fb51ee7866e34c997908626f
10Shiro <160009199+10Shiro@users.noreply.github.com>
2024-03-21 06:56:57 +0000

    Slight redundancy removal
    instead of adding variable show, changed to "show"

--- includes/MaterialsPage.js
+++ includes/MaterialsPage.js
@@ -15,29 +15,11 @@ function filterSubject(subject) {
     }
     
     for (var i = 0; i < allElements.length; i++) {
-        RemoveItem(allElements[i], "show");
+        RemoveItem(allElements[i]);
         if (allElements[i].className.indexOf(subject) > -1) {
-            AddItem(allElements[i], "show")
+            AddItem(allElements[i])
         }
     }
-}
-
-function AddItem(element, show) {
-    var elementClasses;
-    elementClasses = element.className.split(" ");
-    if (elementClasses.indexOf(show == -1)) {
-        element.className += " " + show;
-    }
-    
-}
-
-function RemoveItem (element, show) {
-    var elementClasses;
-    elementClasses = element.className.split(" ");
-    while (elementClasses.indexOf(show) > -1) {
-        elementClasses.splice(elementClasses.indexOf(show), 1)
-    }
-    element.className = elementClasses.join(" ")
 }
 
 function filterStatus(status) {
@@ -46,16 +28,34 @@ function filterStatus(status) {
     allStatus = document.getElementsByClassName("status");
     for (var i = 0; i < allElements.length; i++) {
         if (allElements[i].className.indexOf("show") > -1) {
-            RemoveItem(allElements[i], "show")
+            RemoveItem(allElements[i])
             if (status == "any") {
-                AddItem(allElements[i], "show")
+                AddItem(allElements[i])
             }
             else if (status == "finished" && allStatus[i].checked) {
-                AddItem(allElements[i], "show")
+                AddItem(allElements[i])
             }
             else if (status == "unfinished" && !allStatus[i].checked) {
-                AddItem(allElements[i], "show")
+                AddItem(allElements[i])
             }
         }
     }
+}
+
+function AddItem(element) {
+    var elementClasses;
+    elementClasses = element.className.split(" ");
+    if (elementClasses.indexOf("show" == -1)) {
+        element.className += " show";
+    }
+    
+}
+
+function RemoveItem (element) {
+    var elementClasses;
+    elementClasses = element.className.split(" ");
+    while (elementClasses.indexOf("show") > -1) {
+        elementClasses.splice(elementClasses.indexOf("show"), 1)
+    }
+    element.className = elementClasses.join(" ")
 }
